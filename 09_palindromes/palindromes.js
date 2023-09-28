3c657794472b6481e26c1e084286ad0c629614af
matejmich <matejmich@seznam.cz>
2023-09-28 10:25:59 +0200

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+newStr = str.toLowerCase().replace(/[^a-z0-9]/g,"")
+if (newStr === newStr.split("").reverse().join("")) {
+    return true
+}
+else {
+    return false
+}
 };
 
 // Do not edit below this line
