70c3f196abe593f7378d9fd2135d6d43720fcaab
matejmich <matejmich@seznam.cz>
2023-09-28 11:35:42 +0200

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    a = 0 + a
+    let p = 1;
+    let c = 1;
+    let n;
+if (a <= 0) {
+    return "OOPS"
+}
+if (a <= 2) {
+    return 1
+}
+else {
+    for (i = 2; i < a; i++) {
+        n = p + c;
+        p = c;
+        c = n;
+        
+    }
+    return n;
+}
 
 };
 
